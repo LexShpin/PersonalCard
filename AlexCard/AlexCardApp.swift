12d152dbd86fdda26a9f332f6f304fe256163809
Alexander Shpin <@lexhpin@gmail.com>
2022-09-15 10:34:52 -0500

    Initial Commit

---
+++ AlexCard/AlexCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlexCardApp.swift
+//  AlexCard
+//
+//  Created by Alexander Shpin on 15.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct AlexCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
